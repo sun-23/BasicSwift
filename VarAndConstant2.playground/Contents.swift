4d833aef60ca751182e9c57b9b0721426b9f9c60
sun <twinsamson@gmail.com>
2019-04-16 11:28:08 +0700

    Variable and Constant

---
+++ VarAndConstant2.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var myName = "sun"
+var myNumber = 1234
+var myNumber2 = 3.1416
+var status = true
+
+var mySurname: String = "abe"
+var myNubber3: Int = Int(555.5)
+var myNumber4: Double = 12.34
+var status2: Bool = false
+
+
+
